e546e7837ee9e8102a923be2e096fc2cbf3eb935
pivil <remi.pontonnier@epitech.eu>
2022-05-28 17:16:52 +0200

    get nearest color name by rgb values

--- ColorService.py
+++ ColorService.py
@@ -1,5 +1,11 @@
 import numpy as np
 from sklearn.cluster import KMeans
+from scipy.spatial import KDTree
+from webcolors import (
+    CSS2_HEX_TO_NAMES,
+    hex_to_rgb,
+)
+
 
 class ColorService:
     def get_palette(self, clusters):
@@ -22,10 +28,22 @@ class ColorService:
         clt = KMeans(n_clusters=n)
         return clt.fit(image.reshape(-1, 3))
 
+    def test(self):
+        print('aa')
+    def convert_rgb_to_names(self, rgb_tuple):
+        # a dictionary of all the hex and their respective names in css3
+        css3_db = CSS2_HEX_TO_NAMES
+        names = []
+        rgb_values = []
+        for color_hex, color_name in css3_db.items():
+            names.append(color_name)
+            rgb_values.append(hex_to_rgb(color_hex))
+
+        kdt_db = KDTree(rgb_values)
+        distance, index = kdt_db.query(rgb_tuple)
+        return f'closest match: {names[index]}'
+
     def get_n_width_palette(self, n, image):
         clt = self.get_kmean_cluster(n, image)
         return self.get_palette(clt)
 
-    def draw_n_palette_width(self, n, image):
-        clt = self.get_kmean_cluster(n, image)
-        self.draw_image(self.get_palette(clt))
